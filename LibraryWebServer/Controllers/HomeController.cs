9219a0a26f780fd675aee1055002a434b8b0c70f
Wilson Pan <w1252801556@gmail.com>
2023-03-21 19:56:00 -0600

    Finish the assignment, maybe could do some double check.

--- LibraryWebServer/Controllers/HomeController.cs
+++ LibraryWebServer/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using LibraryWebServer.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Xml.Linq;
 
 namespace LibraryWebServer.Controllers
 {
@@ -17,121 +18,190 @@ namespace LibraryWebServer.Controllers
     private readonly ILogger<HomeController> _logger;
 
 
-    /// <summary>
-    /// Given a Patron name and CardNum, verify that they exist and match in the database.
-    /// If the login is successful, sets the global variables "user" and "card"
-    /// </summary>
-    /// <param name="name">The Patron's name</param>
-    /// <param name="cardnum">The Patron's card number</param>
-    /// <returns>A JSON object with a single field: "success" with a boolean value:
-    /// true if the login is accepted, false otherwise.
-    /// </returns>
-    [HttpPost]
-    public IActionResult CheckLogin( string name, int cardnum )
-    {
-      // TODO: Fill in. Determine if login is successful or not.
-      bool loginSuccessful = false;
-
-      if ( !loginSuccessful )
-      {
-        return Json( new { success = false } );
-      }
-      else
-      {
-        user = name;
-        card = cardnum;
-        return Json( new { success = true } );
-      }
-    }
+        /// <summary>
+        /// Given a Patron name and CardNum, verify that they exist and match in the database.
+        /// If the login is successful, sets the global variables "user" and "card"
+        /// </summary>
+        /// <param name="name">The Patron's name</param>
+        /// <param name="cardnum">The Patron's card number</param>
+        /// <returns>A JSON object with a single field: "success" with a boolean value:
+        /// true if the login is accepted, false otherwise.
+        /// </returns>
+        [HttpPost]
+        public IActionResult CheckLogin(string name, int cardnum)
+        {
+            bool loginSuccessful = false;
+            using (Team100LibraryContext db = new Team100LibraryContext())
+            {
+                var query =
+                   from p in db.Patrons
+                   where p.Name == name && p.CardNum == cardnum
+                   select p;
+                
+                if (query.Count() > 0)
+                    loginSuccessful = true;
+            }
 
 
-    /// <summary>
-    /// Logs a user out. This is implemented for you.
-    /// </summary>
-    /// <returns>Success</returns>
-    [HttpPost]
-    public ActionResult LogOut()
-    {
-      user = "";
-      card = -1;
-      return Json( new { success = true } );
-    }
-
-    /// <summary>
-    /// Returns a JSON array representing all known books.
-    /// Each book should contain the following fields:
-    /// {"isbn" (string), "title" (string), "author" (string), "serial" (uint?), "name" (string)}
-    /// Every object in the list should have isbn, title, and author.
-    /// Books that are not in the Library's inventory (such as Dune) should have a null serial.
-    /// The "name" field is the name of the Patron who currently has the book checked out (if any)
-    /// Books that are not checked out should have an empty string "" for name.
-    /// </summary>
-    /// <returns>The JSON representation of the books</returns>
-    [HttpPost]
-    public ActionResult AllTitles()
-    {
-
-      // TODO: Implement
-
-      return Json( null );
-
-    }
-
-    /// <summary>
-    /// Returns a JSON array representing all books checked out by the logged in user 
-    /// The logged in user is tracked by the global variable "card".
-    /// Every object in the array should contain the following fields:
-    /// {"title" (string), "author" (string), "serial" (uint) (note this is not a nullable uint) }
-    /// Every object in the list should have a valid (non-null) value for each field.
-    /// </summary>
-    /// <returns>The JSON representation of the books</returns>
-    [HttpPost]
-    public ActionResult ListMyBooks()
-    {
-      // TODO: Implement
-      return Json( null );
-    }
+            if (!loginSuccessful)
+            {
+                return Json(new { success = false });
+            }
+            else
+            {
+                user = name;
+                card = cardnum;
+                return Json(new { success = true });
+            }
+        }
 
 
-    /// <summary>
-    /// Updates the database to represent that
-    /// the given book is checked out by the logged in user (global variable "card").
-    /// In other words, insert a row into the CheckedOut table.
-    /// You can assume that the book is not currently checked out by anyone.
-    /// </summary>
-    /// <param name="serial">The serial number of the book to check out</param>
-    /// <returns>success</returns>
-    [HttpPost]
-    public ActionResult CheckOutBook( int serial )
-    {
-      // You may have to cast serial to a (uint)
+        /// <summary>
+        /// Logs a user out. This is implemented for you.
+        /// </summary>
+        /// <returns>Success</returns>
+        [HttpPost]
+        public ActionResult LogOut()
+        {
+            user = "";
+            card = -1;
+            return Json(new { success = true });
+        }
+
+        /// <summary>
+        /// Returns a JSON array representing all known books.
+        /// Each book should contain the following fields:
+        /// {"isbn" (string), "title" (string), "author" (string), "serial" (uint?), "name" (string)}
+        /// Every object in the list should have isbn, title, and author.
+        /// Books that are not in the Library's inventory (such as Dune) should have a null serial.
+        /// The "name" field is the name of the Patron who currently has the book checked out (if any)
+        /// Books that are not checked out should have an empty string "" for name.
+        /// </summary>
+        /// <returns>The JSON representation of the books</returns>
+        [HttpPost]
+        public ActionResult AllTitles()
+        {
+            using(Team100LibraryContext db = new Team100LibraryContext())
+            {
+                var query =
+                    from t in db.Titles
+                    join i in db.Inventory on t.Isbn equals i.Isbn into inv
+                    from j1 in inv.DefaultIfEmpty()
+                    join c in db.CheckedOut on j1.Serial equals c.Serial into chkout
+                    from j2 in chkout.DefaultIfEmpty()
+                    join p in db.Patrons on j2.CardNum equals p.CardNum into list
+                    from j3 in list.DefaultIfEmpty()
+                    select new
+                    {
+                        ISBN = t.Isbn,
+                        Title = t.Title,
+                        Author = t.Author,
+                        Serial = j1 == null ? null : (uint?)j1.Serial,
+                        Name = j3 == null ? "" : j3.Name
+                    };
+                
+                return Json(query.ToArray());
+            }
+        }
+
+        /// <summary>
+        /// Returns a JSON array representing all books checked out by the logged in user 
+        /// The logged in user is tracked by the global variable "card".
+        /// Every object in the array should contain the following fields:
+        /// {"title" (string), "author" (string), "serial" (uint) (note this is not a nullable uint) }
+        /// Every object in the list should have a valid (non-null) value for each field.
+        /// </summary>
+        /// <returns>The JSON representation of the books</returns>
+        [HttpPost]
+        public ActionResult ListMyBooks()
+        {
+            using (Team100LibraryContext db = new Team100LibraryContext())
+            {
+                var query =
+                    from t in db.Titles
+                    join i in db.Inventory on t.Isbn equals i.Isbn into inv
+                    from j1 in inv.DefaultIfEmpty()
+                    join c in db.CheckedOut on j1.Serial equals c.Serial into chkout
+                    from j2 in chkout.DefaultIfEmpty()
+                    join p in db.Patrons on j2.CardNum equals p.CardNum where p.Name == user
+                    select new
+                    {
+                        Title = t.Title,
+                        Author = t.Author,
+                        Serial = j1 == null ? null : (uint?)j1.Serial,
+                    };
+
+                return Json(query.ToArray());
+            }
+        }
 
 
-      return Json( new { success = true } );
-    }
+        /// <summary>
+        /// Updates the database to represent that
+        /// the given book is checked out by the logged in user (global variable "card").
+        /// In other words, insert a row into the CheckedOut table.
+        /// You can assume that the book is not currently checked out by anyone.
+        /// </summary>
+        /// <param name="serial">The serial number of the book to check out</param>
+        /// <returns>success</returns>
+        [HttpPost]
+        public ActionResult CheckOutBook(int serial)
+        {
+            using (Team100LibraryContext db = new Team100LibraryContext())
+            {
+                CheckedOut checkedOut = new CheckedOut();
+                checkedOut.Serial = (uint) serial;
+                checkedOut.CardNum = (uint) card;
 
-    /// <summary>
-    /// Returns a book currently checked out by the logged in user (global variable "card").
-    /// In other words, removes a row from the CheckedOut table.
-    /// You can assume the book is checked out by the user.
-    /// </summary>
-    /// <param name="serial">The serial number of the book to return</param>
-    /// <returns>Success</returns>
-    [HttpPost]
-    public ActionResult ReturnBook( int serial )
-    {
-      // You may have to cast serial to a (uint)
+                db.CheckedOut.Add(checkedOut);
+                try
+                {
+                    db.SaveChanges();
+                }catch(Exception ex)
+                {
+                    return Json(new { success = false });
+                }
+            }
 
-      return Json( new { success = true } );
-    }
+            return Json(new { success = true });
+        }
+
+        /// <summary>
+        /// Returns a book currently checked out by the logged in user (global variable "card").
+        /// In other words, removes a row from the CheckedOut table.
+        /// You can assume the book is checked out by the user.
+        /// </summary>
+        /// <param name="serial">The serial number of the book to return</param>
+        /// <returns>Success</returns>
+        [HttpPost]
+        public ActionResult ReturnBook(int serial)
+        {
+            using (Team100LibraryContext db = new Team100LibraryContext())
+            {
+                CheckedOut checkedOut = new CheckedOut();
+                checkedOut.Serial = (uint) serial;
+                checkedOut.CardNum = (uint) card;
+
+                db.CheckedOut.Remove(checkedOut);
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    return Json(new { success = false });
+                }
+            }
+                return Json(new { success = true });
+        }
 
 
-    /*******************************************/
-    /****** Do not modify below this line ******/
-    /*******************************************/
+        /*******************************************/
+        /****** Do not modify below this line ******/
+        /*******************************************/
 
 
-    public IActionResult Index()
+        public IActionResult Index()
     {
       if ( user == "" && card == -1 )
         return View( "Login" );
